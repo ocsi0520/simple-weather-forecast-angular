d466fa6c33ec42a6af33fc6b7a1c6daa31c2809f
Faragó Tamás <ocsi0520@gmail.com>
2021-12-08 15:07:45 +0100

    cache response in http-weather-service

--- Weather/ClientApp/src/app/list/service/http-weather-service.ts
+++ Weather/ClientApp/src/app/list/service/http-weather-service.ts
@@ -1,16 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 import { ListViewQueryDescriptor } from '../descriptors/list-view-query-descriptor';
 import { WeatherForecast } from '../weather-forecast';
 import { WeatherResponseMapper } from './response-mapper/response-mapper';
 import { WeatherService } from './weather-service';
 import { WeatherResponse } from "./weather-response";
 
-// TODO: create a simple cache, when uploading a new csv clear it
 @Injectable()
 export class HttpWeatherService implements WeatherService {
+  private static cachedForeCasts: Array<WeatherForecast> | null = null;
   constructor (
     private http: HttpClient,
     @Inject('BASE_URL') private baseUrl: string,
@@ -19,18 +19,24 @@ export class HttpWeatherService implements WeatherService {
   public uploadWeatherList(file: File): Observable<void> {
     const formData = new FormData();
     formData.append('file', file);
-  
-    // angular does not provide proper overlod for post method
+
+    // angular does not provide proper overload for post method
     // that's why we need responseType 'text', otherwise it will
     // automatically try to parse a json, which results in error
     return this.http.post<void>(this.getWeatherApiUrl(), formData, {
       responseType: 'text' as any
-    });
+    }).pipe(tap(() => HttpWeatherService.cachedForeCasts = null));
   }
   public getWeatherListView(listViewDescriptor?: ListViewQueryDescriptor): Observable<WeatherResponse> {
-    return this.http.get<WeatherForecast[]>(this.getWeatherApiUrl()).pipe(
-      map((weatherForecasts) => this.responseMapper.mapRawResponse(listViewDescriptor, weatherForecasts))
-    );
+    const mapperOperator =
+      map((weatherForecasts: Array<WeatherForecast>) => this.responseMapper.mapRawResponse(listViewDescriptor, weatherForecasts));
+    if (HttpWeatherService.cachedForeCasts !== null)
+      return of(HttpWeatherService.cachedForeCasts).pipe(mapperOperator);
+    else
+      return this.http.get<WeatherForecast[]>(this.getWeatherApiUrl()).pipe(
+        tap(rawResponse => HttpWeatherService.cachedForeCasts = rawResponse),
+        mapperOperator
+      );
   }
 
   private getWeatherApiUrl(): string {
